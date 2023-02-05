23401a13cdf678c5131bf05b591a6a7e183faca4
Ritesh sute <suteritesh@gmail.com>
2023-02-06 00:50:50 +0530

    passwordEncryption

--- expenseController/expense.js
+++ expenseController/expense.js
@@ -1,45 +1,53 @@
 const User = require('../model/expense');
+const Bcrypt = require('bcrypt')
 
-exports.signUpUser = ('/expense/signup', async (req, res, next) => {
-    const userData = req.body;
-
-    console.log(userData)
-
-    const existingUser = await User.findOne({ where: { email: userData.email } });
-    
-    if (existingUser) {
-      return res.status(400).json({
-        error: 'User already exists'
+exports.signUpUser = ( async (req, res) => {
+    const { name, email, password, phonenumber} = req.body;
+    // console.log(userData)
+    const existingUser = await User.findOne({ where: { email: email } });
+    try{
+      if (existingUser) {
+        return res.status(400).json({
+          error: 'User already exists'
+        });
+      } 
+        Bcrypt.hash(password, 10, async(err, hash) => {
+          console.log(err)
+          const user = await User.create({name, email, password: hash, phonenumber});
+        
+          res.status(201).json({
+          message: 'User created successfully'
+        })
       });
-    } 
-    
-    const user = await User.create(userData);
-
-    res.status(201).json({
-      message: 'User created successfully'
-    });
+    } catch(err) {
+      res.status(500).json(err)
+     }
   })
 
 
-  exports.loginUser =  ('/expense/login', async (req, res, next) => {
+  exports.loginUser =  ( async (req, res) => {
+    try{
     const { email, password } = req.body;
-    console.log()
-  
 
-
-    User.findOne({ where: { email, password }  })
-      .then(user => {
-        if (!user) {
-          return res.status(400).json({ message: "User not found" });
+    const user = await User.findAll({ where: { email }})
+    console.log(user)
+        if (user.length > 0) {
+          Bcrypt.compare(password, user[0].password, (err, result) => {
+            if(err){
+              throw new Error('Something Went Wrong');
+            } 
+            if(result === true){
+              res.status(200).json({ success: true, message: "User logged in successfully" });
+            } else {
+              return res.status(400).json({ success: false, message: "Password not matched" });
+            }
+          })
+        } else {
+           return res.status(404).json({ success: false, message: "User not found" });
         }
-        // else if (password === password){
-        //   console.log('password matched');
-        //   return res.status(201).json({ message: "password matched" })
-        // }
-        res.json({ message: "Login successful" });
-      })
-      .catch(error => {
-        res.status(404).json({ message: error.message + 'user not found'});
-      });
-  });
+    } catch(err) {
+      res.status(500).json({success: false, message: err} )
+    }
+  })
+  
   
